64f3057ac5dda9c558cea47304a53885192b64f9
Dewey <hammersdrichard@gmail.com>
2021-01-18 11:40:23 -0800

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -21,6 +21,35 @@ class Board
         puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
     end
 
+    def player_move(input)
+        input.to_i - 1
+    end
+
+    def cells_count
+        cells.grep(/X|O/).length
+    end
+
     def position(input)
+        @cells[player_move(input)]
+    end
+
+    def full?
+        cells_count() == 9 
+    end
+
+    def turn_count
+        cells_count()
+    end
+
+    def taken?(input)
+        /X|O/.match?(cells[player_move(input)])
+    end
+
+    def valid_move?(input)
+        /^[0-8]$/.match?(player_move(input).to_s) && !taken?(input)
+    end
+
+    def update(position, player)
+        @cells[player_move(position)] = player.token if valid_move?(position) 
     end
 end
